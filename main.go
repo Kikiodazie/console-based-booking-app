da8ddeed8055548b4593540d475d4e56888f5c80
kikiodazie <dodazie@gmail.com>
2022-05-24 19:34:12 +0100

    :wq

--- main.go
+++ main.go
@@ -74,20 +74,4 @@ func main() {
 			continue
 		}
 	}
-
-	// Switch statement example
-	city := "London"
-
-	switch city {
-	case "New York":
-		// booking for New York conference
-	case "Singapore", "Hong Kong":
-		// booking for Singapore & Hong Kong conference
-	case "London", "Berlin":
-		// booking for London & Berlin conference
-	case "Mexico City":
-		// booking for Mexico City conference
-	default:
-		fmt.Print("No valid city selected")
-	}
 }
